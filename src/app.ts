e2330e965cb84f84c5a210cc6ae6c8ea99e1708e
ahmadparvej <ahmadparvej58@gmail.com>
2024-11-02 21:51:22 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+ 
+app.use((err: HttpError, req: Request, res: Response) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        message: err.message,
+        type: err.name,
+        location: "",
+      },
+    ],
+  });
 });
 
 export default app;
