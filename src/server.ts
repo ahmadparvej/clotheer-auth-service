68a34ae3b56f711affb3e01ad82b24e771d94bf0
ahmadparvej <ahmadparvej58@gmail.com>
2024-11-02 20:40:43 +0530

    add config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import { Config } from "./config/index";
+
 function welcome(name: string) {
-  console.log("Welcome to the server!", name);
+  console.log("Welcome to the server!", name, Config.PORT);
 }
 
 welcome("parvej");
