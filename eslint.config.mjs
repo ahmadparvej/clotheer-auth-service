b801bc081c33084aa0622e3f50a3ce5316c4d6e3
ahmadparvej <ahmadparvej58@gmail.com>
2024-11-03 08:52:56 +0530

    add jest testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
-    ignores: ["node_modules", "dist", "eslint.config.mjs"],
+    ignores: ["node_modules", "dist", "eslint.config.mjs", "jest.config.js"],
   },
   {
     languageOptions: {
@@ -18,6 +18,7 @@ export default tseslint.config(
     },
     rules: {
       "no-console": "warn",
+      "@typescript-eslint/no-misused-promises": "off",
     },
   },
 );
