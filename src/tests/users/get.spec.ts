5d0c8c9f563351d30a933e106713e6c8e7ed0373
ahmadparvej <ahmadparvej58@gmail.com>
2024-11-20 09:36:38 +0530

    add timeout users test

--- src/tests/users/get.spec.ts
+++ src/tests/users/get.spec.ts
@@ -17,7 +17,7 @@ describe("GET /users", () => {
   beforeAll(async () => {
     jwks = createJWKSMock("http://localhost:5501");
     connection = await AppDataSource.initialize();
-  });
+  }, 10000);
 
   beforeEach(async () => {
     jwks.start();
@@ -26,12 +26,12 @@ describe("GET /users", () => {
     // database truncate
     await connection.dropDatabase();
     await connection.synchronize();
-  });
+  }, 10000);
 
   afterAll(async () => {
     jwks.stop();
     await connection.destroy();
-  });
+  }, 10000);
 
   describe("Given all fields", () => {
     it("should return a list of users", async () => {
